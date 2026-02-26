a0da595b02f1eef3ea3ed634514de1fcc1cfab9a
gigante <viniciusgigante539@gmail.com>
2026-02-26 10:04:42 -0300

    fix: install.sh é executavel em qualquer usuario e caminho

--- TF01/scripts/install.sh
+++ TF01/scripts/install.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# Definindo a raiz do projeto dinamicamente
+BASE_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+
 echo "Atualizando pacotes..."
 sudo apt update
 
@@ -8,22 +13,22 @@ echo "Removendo configuração default..."
 sudo rm -f /etc/nginx/sites-enabled/default
 
 echo "Copiando arquivos do site..."
-sudo cp -r website/* /usr/share/nginx/html/
+sudo cp -r "$BASE_DIR/website/"* /usr/share/nginx/html/
 
 echo "Copiando configuração personalizada..."
-sudo cp nginx/site.conf /etc/nginx/sites-available/empresa
+sudo cp "$BASE_DIR/nginx/site.conf" /etc/nginx/sites-available/empresa
 
 echo "Ativando virtual host..."
 sudo ln -sf /etc/nginx/sites-available/empresa /etc/nginx/sites-enabled/
 
-echo "Definindo permissões"
+echo "Definindo permissões..."
 sudo chown -R www-data:www-data /usr/share/nginx/html
 sudo chmod -R 755 /usr/share/nginx/html
 
-echo "Habilitando Nginx no boot"
+echo "Habilitando Nginx no boot..."
 sudo systemctl enable nginx
 
-echo "Reiniciando serviço"
+echo "Reiniciando serviço..."
 sudo systemctl restart nginx
 
 echo "Deploy concluído! Acesse: http://localhost"
